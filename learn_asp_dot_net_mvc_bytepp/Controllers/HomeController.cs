a051ca151ea4f7d404932d7bcd0f7594a8126318
whoami <az.khabibullin2018@gmail.com>
2024-06-25 10:26:08 +0500

    урок клиент сервер приложение на асп дот нет

---
+++ learn_asp_dot_net_mvc_bytepp/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace learn_asp_dot_net_mvc_bytepp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View("Default");
+        }
+
+        [HttpPost]
+        public string Hello(string name)
+        {
+            return $"Hello {name}";
+
+            //return string.Format("Hello {0}", name);
+        }
+    }
+}
