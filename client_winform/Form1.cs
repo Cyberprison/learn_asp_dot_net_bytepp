f2a0a0dbd2fbbbe544bf74bd987c61b9d7216acb
whoami <az.khabibullin2018@gmail.com>
2024-06-27 23:44:07 +0500

    обращение к контроллеру по юрл адресу локального сервера сервер для примера принимает в виде json, её надо будет декодировать, потом составим ответ и закодируем его и отправим обратно на клиент клиент при нажании на кнопку кодирует сообщение из текст бокса в json. Объект webRequest для запроса к контроллеру. Если будет удаленный сервер, то за место локального адреса надо написать доменое имя. Указали метод запроса, тело запроса в виде json, нужно далее закодировать в массив байтов. Указали тип контента передаваемого на сервак, длину контента. Запишем тело запроса в поток запроса. После запроса получаем ответ в объекте webResponse, присвоить ему значение ответа. Ответ от контроллера записан в answer, и считываем поток. Закрываем ответ и вывод в диалоговом окне.

--- client_winform/Form1.cs
+++ client_winform/Form1.cs
@@ -5,8 +5,12 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
 
 namespace client_winform
 {
@@ -15,6 +19,46 @@ namespace client_winform
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //почему асинк
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string jsonData = await Task.Factory.StartNew(() => JsonConvert.SerializeObject(textBox1.Text));
+
+            //http://localhost:51437/Home/Hello
+            WebRequest webRequest = WebRequest.Create("http://localhost:51437/Home/Hello");
+
+            webRequest.Method = "POST";
+
+            string query = $"name={jsonData}";
+
+            byte[] byteMessage = Encoding.UTF8.GetBytes(query);
+
+            webRequest.ContentType = "application/x-www-form-urlencoded";
+
+            webRequest.ContentLength = byteMessage.Length;
+
+            using(Stream stream = await webRequest.GetRequestStreamAsync())
+            {
+                await stream.WriteAsync(byteMessage, 0, byteMessage.Length);
+            }
+
+            WebResponse webResponse = await webRequest.GetResponseAsync();
+
+            string answer = null;
+
+            using (Stream s = webResponse.GetResponseStream())
+            {
+                using (StreamReader sR = new StreamReader(s))
+                {
+                    answer = await sR.ReadToEndAsync();
+                }
+            }
+            
+            webResponse.Close();
+
+            MessageBox.Show(answer);
         }
     }
 }
--- learn_asp_dot_net_mvc_bytepp/Controllers/HomeController.cs
+++ learn_asp_dot_net_mvc_bytepp/Controllers/HomeController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
 namespace learn_asp_dot_net_mvc_bytepp.Controllers
 {
     public class HomeController : Controller
@@ -14,12 +17,19 @@ namespace learn_asp_dot_net_mvc_bytepp.Controllers
             return View("Default");
         }
 
+        //почему асинхронный метод должен быть?
         [HttpPost]
-        public string Hello(string name)
+        public async Task<string> Hello(string name)
         {
-            return $"Hello {name}";
+            //string userName = await JsonConvert.DeserializeObject<string>(name);
+
+            string userName = await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<string>(name));
+
+            //return $"Hello {name}";
 
             //return string.Format("Hello {0}", name);
+
+            return await Task.Factory.StartNew(() => JsonConvert.SerializeObject(string.Format("Hello {0}", userName)));
         }
     }
 }
